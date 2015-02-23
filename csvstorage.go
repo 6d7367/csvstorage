e674ba3218b6de228d1e5dc78744f3b5808fb1d4
user <6d7367@gmail.com>
2015-02-23 07:02:36 +0200

    csvstorage: добавил вычисление простого where-условия

--- csvstorage.go
+++ csvstorage.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 type CSVTableSchemaRecord struct {
@@ -30,9 +31,83 @@ func ReadSchema(r io.Reader) CSVTableSchema {
 	return schema
 }
 
+type WhereCondition interface {
+	Compute([]string, CSVTableSchema) bool
+}
+
+type WhereSimpleCondition struct {
+	field, op, value string
+}
+
+func NewSimpleWhere(field, op, value string) WhereSimpleCondition {
+	c := WhereSimpleCondition{
+		field: field,
+		op:    op,
+		value: value,
+	}
+
+	return c
+}
+
+func (this WhereSimpleCondition) Compute(record []string, schema CSVTableSchema) bool {
+
+	testInt := func(op string, value, test int) bool {
+		switch op {
+		case "=":
+			return value == test
+		case "!=":
+			return value != test
+		case "<":
+			return value < test
+		case ">":
+			return value > test
+		case "<=":
+			return value <= test
+		case ">=":
+			return value >= test
+		}
+
+		return false
+	}
+
+	testStr := func(op, value, test string) bool {
+		switch op {
+		case "=":
+			return value == test
+		case "!=":
+			return value != test
+		case "<":
+			return value < test
+		case ">":
+			return value > test
+		case "<=":
+			return value <= test
+		case ">=":
+			return value >= test
+		}
+
+		return false
+	}
+
+	fieldIndex := schema[this.field].index
+
+	switch schema[this.field].recordType {
+	case "int":
+		fieldValue, _ := strconv.ParseInt(record[fieldIndex], 10, 32)
+		testValue, _ := strconv.ParseInt(this.value, 10, 32)
+		return testInt(this.op, int(fieldValue), int(testValue))
+	case "string":
+		return testStr(this.op, record[fieldIndex], this.value)
+	}
+
+	return false
+}
+
 type SelectQuery struct {
 	fields       []string
 	table        string
+	where        WhereCondition
+	limit        int
 	schema       CSVTableSchema
 	fieldsToRead []int
 }
@@ -65,6 +140,18 @@ func (this *SelectQuery) From(table string) *SelectQuery {
 	return this
 }
 
+func (this *SelectQuery) Where(cond WhereCondition) *SelectQuery {
+	this.where = cond
+
+	return this
+}
+
+func (this *SelectQuery) Limit(limit int) *SelectQuery {
+	this.limit = limit
+
+	return this
+}
+
 func (this *SelectQuery) Do() SelectQueryResult {
 	r, _ := os.Open(this.table + ".csv")
 
@@ -75,17 +162,31 @@ func (this *SelectQuery) Do() SelectQueryResult {
 	result := make(SelectQueryResult, 0)
 
 	for _, v := range records {
-		resultRecord := make(SelectQueryResultRecord)
-		for _, fieldIndex := range this.fieldsToRead {
-
-			resultRecord[fieldIndex] = v[fieldIndex]
+		if len(result) >= this.limit && this.limit != 0 {
+			break
 		}
-		result = append(result, resultRecord)
+
+		if this.where.Compute(v, this.schema) {
+			resultRecord := make(SelectQueryResultRecord)
+			for _, fieldIndex := range this.fieldsToRead {
+
+				resultRecord[fieldIndex] = v[fieldIndex]
+			}
+			result = append(result, resultRecord)
+		}
+
 	}
 
 	return result
 }
 
 func main() {
-	fmt.Println(Select("content", "id").From("index").Do())
+	simpleWhere := NewSimpleWhere("id", "!=", "1")
+	q := Select("content", "id").From("index").Limit(3).Where(simpleWhere)
+	for _, r := range q.Do() {
+		for _, f := range r {
+			fmt.Print(f, " ")
+		}
+		fmt.Println()
+	}
 }
